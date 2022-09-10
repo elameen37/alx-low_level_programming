ceb0247be870d37ac52f33242a2837bb8e9d6ed6
elameen37 <elameen37@gmail.com>
2022-09-10 02:47:53 +0000

    commit for variables task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Prints alphabets in lowercase followed by a new line
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+	
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	
+	putchar(lowercase);
+	putchar('\n');
+
+	return (0);
+}
+	
