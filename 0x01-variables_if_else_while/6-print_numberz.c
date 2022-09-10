d07763b4b95d2c8d37e92ab12e9d0de2cf1a053e
elameen37 <elameen37@gmail.com>
2022-09-10 04:07:12 +0000

    commit for variables task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0, 
+ * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line.
  *
  * Return: Always 0 (Success)
@@ -11,7 +11,7 @@ int main(void)
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-  putchar(d);
+	putchar(d);
 	putchar('\n');
 
 	return (0);
