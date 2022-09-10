5408a4f22ded0b46fe4219de23156cbed6b2dc0c
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-09 20:53:56 -0700

    commit variable task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
