fb8768c8c892c8e0c780aa071aa32684a7193e05
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-09 21:03:08 -0700

    commit variables task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, 
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+  putchar(d);
+	putchar('\n');
+
+	return (0);
+}
