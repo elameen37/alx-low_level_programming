0ef08b19c8b0a9d54d9ee57ae44bb5821235e3f7
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-09 21:57:00 -0700

    update variables task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,21 +7,30 @@
  */
 int main(void)
 {
-	int d;
+	int d, p, q;
 
-	for (d = 0; d < 90; d++)
+	for (d = '0'; d < '9'; d++)
 	{
-		for ()
+		for (p = d + 1; p <= '9'; p++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+
+					if (d == '7' && p == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
