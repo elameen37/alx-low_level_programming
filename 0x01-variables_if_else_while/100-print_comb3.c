c8237d9ffd368c8ed139b1d3e210b0a84bca3510
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-09 22:03:06 -0700

    update commit task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,23 @@
  */
 int main(void)
 {
-	int d;
+	int d, p;
 
-	for (d = 0; d < 100; d++)
+	for (d = '0'; d < '9'; d++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
+		for (p = d + 1; p <= '9'; p++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (p != d)
+			{
+				putchar(d);
+				putchar(p);
+
+				if (d == '8' && p == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
