9ebe3e2cfaf981b36c6748af15458a8f359fee01
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-13 16:50:55 -0700

    commit functions nested loops task4

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check for alphabetic character
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
