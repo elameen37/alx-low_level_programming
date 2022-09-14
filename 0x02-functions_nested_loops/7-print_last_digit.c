1de6ad97a8bcf3f09ff078bf035f650cd121540f
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-13 18:27:45 -0700

    commit functions nested loops task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @nld: number's last digit result
+ * Return: value of the last digit
+ */
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld);
+}
+Footer
+© 2022 GitHub, Inc.
