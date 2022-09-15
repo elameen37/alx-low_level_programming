68b54ae2abe6d44afc6a8cda8a7a9693ef33882e
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-15 08:03:03 -0700

    commit task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9.
+ * Do not print 2 and 4.
+ * followed by a new line.
+ * Return: Always 0
+ */
+ void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+
+		i++;
+	}
+	_putchar('\n');
+}
