e632c7ccb2b678d9afea1857d3fb0107ce92b144
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-09-28 06:24:38 -0700

    commit task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+	if (*s == '\0')
+		return (0);
+
+	len = 1 +  _strlen_recursion(s + 1);
+
+	return (len);
+}
