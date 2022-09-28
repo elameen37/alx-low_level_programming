fdbfa3fba8d84f92941d48930ae09d4a4b5ccc06
elameen37 <elameen37@gmail.com>
2022-09-28 15:10:26 +0000

    commit task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,23 +3,23 @@
 /**
  * _sqrt_recursion - return the natural square root of a number
  * @n: int number
- * Return: If no natural square root, return -1. Else return natural
+ * Return: If no natural square root, return -1. else return natural
  * square root
  */
 
 int _sqrt_recursion(int n)
 {
-	return (halp(n, 1));
+	return (help(n, 1));
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
+ * help - helper function to solve _sqrt_recursion
  * @c: number to determine if square root
  * @i: incrementer to compare against `c`
  * Return: square root if natural square root, or -1 if none found
  */
 
-int halp(int c, int i)
+int help(int c, int i)
 {
 	int root;
 
@@ -27,7 +27,7 @@ int halp(int c, int i)
 	if (root == c)
 		return (i);
 	else if (root < c)
-		return (halp(c, i + 1));
+		return (help(c, i + 1));
 	else
 		return (-1);
 }
