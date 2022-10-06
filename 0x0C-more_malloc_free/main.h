9731acc084ae11b8c4fb0ad9f8bf23c67dc10b4c
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-10-06 06:56:09 -0700

    update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,6 @@
-#ifndef /*MAIN_H*/
-#define /*MAIN_H*/
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
