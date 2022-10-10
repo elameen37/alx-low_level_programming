54fb53dc15821840780aa9afb51f7e7ab2f94ffb
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-10-10 06:01:43 -0700

    commit task0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,16 +1,3 @@
-#include "0-object_like_macro.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int s;
-
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
-}
+#ifndef SIZE
+#define SIZE 1024
+#endif
