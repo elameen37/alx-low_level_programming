ce81e73b2a4d53b3726ecb6c2ac1cac068fb08f2
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-10-20 05:58:46 -0700

    commit task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Print all the elements of a list_t list
+ * @h: data type pointer of struct
+ * Return: elements of the str index
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t index;
+	const list_t *tmp;
+
+	index = 0;
+	tmp = h;
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			tmp = tmp->next;
+			i++;
+		}
+		else
+		{
+			printf("[%d] %s\n", tmp->len, tmp->str);
+			tmp = tmp->next;
+			index++;
+		}
+	}
+	return (index);
+}
