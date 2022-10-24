0ce19664f166eb54d9a8622027c1ea8713bb95e5
Aminu Shaibu <68182393+elameen37@users.noreply.github.com>
2022-10-24 05:10:50 -0700

    update task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,16 +3,16 @@
 /**
  * listint_len - Return the number of elements
  * @h: data type pointer of struct
- * Return: elements of the str i
+ * Return: elements of the str count
  *  
  * */
 size_t listint_len(const listint_t *h)
 {
-	unsigned int i = 0;
-	const listint_t	 *new_node = h;
+	const listint_t *c;
+	size_t count;
 
-	for (; new_node; new_node = new_node->next)
-	
-		i++;
-		return (i);
+	for (c = h, count = 0; c != NULL; count++, c = c->next)
+		;
+
+	return (count);
 }
