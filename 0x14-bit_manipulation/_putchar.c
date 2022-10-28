7644d047dd470cfdfdedbfd068bbaf7b816e663b
elameen37 <elameen37@gmail.com>
2022-10-28 04:36:40 +0100

    commit _putchar and main.h

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
+ * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
