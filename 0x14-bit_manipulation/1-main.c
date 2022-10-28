4fa26858a7ebfb968c06c33115810a92047e1764
elameen37 <elameen37@gmail.com>
2022-10-28 09:44:00 +0000

    edit main

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the codes.
  *
  * Return: Always 0.
  */
