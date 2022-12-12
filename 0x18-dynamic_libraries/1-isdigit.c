4b9d0c9bab41952d93e95188ed01541c11fd9e27
elameen37 <elameen37@gmail.com>
2022-12-12 22:15:57 +0100

    commit functions

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check the code
+ * @c: type int
+ * Return: 1 if it is a digit 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+
+		return (1);
+
+	return (0);
+}
