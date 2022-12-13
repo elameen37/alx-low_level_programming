33280c651b3757920b3b045987d592f128ced1ed
elameen37 <elameen37@gmail.com>
2022-12-13 01:37:57 +0100

    correct task3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/elameen37/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigalib.so:
-export LD_PRELOAD="$PWD/../gigalib.so"
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/elameen37/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
+export LD_PRELOAD=/tmp/gigalib.so
